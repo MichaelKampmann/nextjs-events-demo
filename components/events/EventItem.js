23dace7dbbe0ea925da8173a8f91d8afea033329
michaelkampmann <post.mkr@icloud.com>
2023-03-04 16:14:33 +0100

    78. Adding Buttons & Icons

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,5 +1,8 @@
-import Link from 'next/link';
+import Button from '../UI/Button';
+import DateIcon from '../icons/DateIcon';
 import classes from './EventItem.module.css';
+import AddressIcon from '../icons/AddressIcon';
+import ArrowRightIcon from '../icons/ArrowRightIcon';
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -19,14 +22,21 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore event</Link>
+          <Button link={exploreLink}>
+            <span>Explore event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
