113d7a019b383a8c41527eecac317d350768613a
michaelkampmann <post.mkr@icloud.com>
2023-03-27 17:39:39 +0200

    134. Merging "head" Content

--- events-demo/pages/_app.js
+++ events-demo/pages/_app.js
@@ -6,6 +6,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <title>Events provided by NextJS</title>
+        <meta name="description" content="NextJS" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
