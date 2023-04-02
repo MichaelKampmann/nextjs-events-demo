efc5ebf65cdeda7f977dafe7a94dcbba20ee264a
michaelkampmann <post.mkr@icloud.com>
2023-04-02 15:38:26 +0200

    147. Using API Routes To Get Data

--- api-routes/pages/api/feedback.js
+++ api-routes/pages/api/feedback.js
@@ -1,6 +1,16 @@
 import fs from 'fs';
 import path from 'path';
 
+function buildFeedbackPath() {
+  return path.join(process.cwd(), 'data', 'feedback.json');
+}
+
+function extractFeedback(filePath) {
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+}
+
 function handler(req, res) {
   if (req.method === 'POST') {
     const email = req.body.email;
@@ -13,9 +23,8 @@ function handler(req, res) {
     };
 
     // Store in database.
-    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
-    const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData);
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
     data.push(newFeedback);
 
     fs.writeFileSync(filePath, JSON.stringify(data));
@@ -24,8 +33,10 @@ function handler(req, res) {
       feedback: newFeedback,
     });
   } else {
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
     res.status(200).json({
-      message: 'Great success!',
+      feedback: data,
     });
   }
 }
