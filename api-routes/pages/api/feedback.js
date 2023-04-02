88cab302ac99b405c15c3bb4b409dee3766d64be
michaelkampmann <post.mkr@icloud.com>
2023-04-02 15:02:39 +0200

    145. Parsing The Incoming Request & Executing Server-side Code

--- api-routes/pages/api/feedback.js
+++ api-routes/pages/api/feedback.js
@@ -1,7 +1,33 @@
+import fs from 'fs';
+import path from 'path';
+
 function handler(req, res) {
-  res.status(200).json({
-    message: 'Great success!',
-  });
+  if (req.method === 'POST') {
+    const email = req.body.email;
+    const feedback = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(), // Lol!
+      email,
+      text: feedback,
+    };
+
+    // Store in database.
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({
+      message: 'success',
+      feedback: newFeedback,
+    });
+  } else {
+    res.status(200).json({
+      message: 'Great success!',
+    });
+  }
 }
 
 export default handler;
