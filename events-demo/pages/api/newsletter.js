6037e7291d5111e696af04d0ff47651c09916585
michaelkampmann <post.mkr@icloud.com>
2023-04-12 17:33:36 +0200

    159. Running MongoDB Queries From Inside API Routes

--- events-demo/pages/api/newsletter.js
+++ events-demo/pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -8,6 +10,15 @@ function handler(req, res) {
       });
       return;
     }
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://nextjsevents:nextjseventspassword@cluster0.ri8crmw.mongodb.net/newsletter?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    await db.collection('emails').insertOne({ email: userEmail });
+
+    client.close();
 
     res.status(201).json({
       message: 'Signed up',
