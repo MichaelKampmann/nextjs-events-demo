2090727440343c126b3366f7eb25f54cd0063419
michaelkampmann <post.mkr@icloud.com>
2023-04-14 09:21:27 +0200

    162. Adding Error Handling

--- events-demo/pages/api/newsletter.js
+++ events-demo/pages/api/newsletter.js
@@ -1,5 +1,18 @@
 import { MongoClient } from 'mongodb';
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://nextjsevents:nextjseventspassword@cluster0.ri8crmw.mongodb.net/events?retryWrites=true&w=majority'
+  );
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  await db.collection('newsletter').insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
@@ -10,15 +23,26 @@ async function handler(req, res) {
       });
       return;
     }
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({
+        message: 'connecting to the database failed',
+      });
+      return;
+    }
 
-    const client = await MongoClient.connect(
-      'mongodb+srv://nextjsevents:nextjseventspassword@cluster0.ri8crmw.mongodb.net/events?retryWrites=true&w=majority'
-    );
-    const db = client.db();
+    try {
+      await insertDocument(client, { email: userEmail });
 
-    await db.collection('newsletter').insertOne({ email: userEmail });
-
-    client.close();
+      client.close();
+    } catch (error) {
+      res.status(500).json({
+        message: 'Inserting data failed',
+      });
+      return;
+    }
 
     res.status(201).json({
       message: 'Signed up',
