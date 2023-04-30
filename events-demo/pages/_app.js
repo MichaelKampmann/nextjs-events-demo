ea9ed11781e66513b4a197f60a5fe88f8188d118
michaelkampmann <post.mkr@icloud.com>
2023-04-30 18:24:01 +0200

    169. Our Target State & Starting Project

--- events-demo/pages/_app.js
+++ events-demo/pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from '../components/layout/Layout';
 import Head from 'next/head';
 import '../styles/globals.css';
+import Notification from '../components/UI/Notification';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Notification title="Test" message="This is a test" status="pending" />
     </Layout>
   );
 }
