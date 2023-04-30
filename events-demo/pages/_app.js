37d235cbef9e371413ccfcd794897e0793ae919d
michaelkampmann <post.mkr@icloud.com>
2023-04-30 19:24:27 +0200

    170. Creating a New React Context

--- events-demo/pages/_app.js
+++ events-demo/pages/_app.js
@@ -2,18 +2,24 @@ import Layout from '../components/layout/Layout';
 import Head from 'next/head';
 import '../styles/globals.css';
 import Notification from '../components/UI/Notification';
+import { NotificationContextProvider } from '../store/notification-context';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Events provided by NextJS</title>
-        <meta name="description" content="NextJS" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-      <Notification title="Test" message="This is a test" status="pending" />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Events provided by NextJS</title>
+          <meta name="description" content="NextJS" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+        <Notification title="Test" message="This is a test" status="pending" />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
